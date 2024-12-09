97b6d83a42ea06f5be5a80d47c402ff073e299ec
Lewo <leventozkan89@gmail.com>
2024-12-09 13:23:57 +0300

    Added support for detecting bluetooth low energy(LE) devices

--- ios/Classes/SwiftHeadsetConnectionEventPlugin.swift
+++ ios/Classes/SwiftHeadsetConnectionEventPlugin.swift
@@ -53,7 +53,7 @@ public class SwiftHeadsetConnectionEventPlugin: NSObject, FlutterPlugin {
         let currentRoute = AVAudioSession.sharedInstance().currentRoute
         for output in currentRoute.outputs {
             let portType = output.portType
-            if portType == AVAudioSession.Port.headphones || portType == AVAudioSession.Port.bluetoothA2DP || portType == AVAudioSession.Port.bluetoothHFP {
+            if portType == AVAudioSession.Port.headphones || portType == AVAudioSession.Port.bluetoothA2DP || portType == AVAudioSession.Port.bluetoothHFP || portType == AVAudioSession.Port.bluetoothLE {
                 return 1
             } else {
                 return 0
